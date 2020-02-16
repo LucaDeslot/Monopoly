ef09a8b10e2371f5519835495dc29af9eae6d766
LucaDeslot <deslot.luca@gmail.com>
2020-02-16 14:31:10 -0500

    Exceptions

---
+++ src/com/exceptions/NombreJoueurException.java
@@ -0,0 +1,8 @@
+package com.exceptions;
+
+public class NombreJoueurException extends Exception {
+	public NombreJoueurException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message); 
+	}
+}
